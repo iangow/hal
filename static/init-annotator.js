e8140b4100c15f89376163bc06485b4eb743ae4c
Andrew Marder <andrew.n.marder@gmail.com>
2015-02-18 12:10:41 -0500

    Figured out how to put selectable list below comments field.

--- static/init-annotator.js
+++ static/init-annotator.js
@@ -14,16 +14,17 @@ Annotator.Plugin.StoreLogger = function (element) {
             console.info("The annotation: %o has just been deleted!", annotation)
           });
 
-var select = $('<select id="my-select" />')
-$('#annotator-field-0').replaceWith(select);
+var select = $('<ol id="my-select" />')
+$('#annotator-field-0').after(select);
 
 var selectValues = { "1": "test 1", "2": "test 2" };
 $.each(selectValues, function(key, value) {   
      $('#my-select')
-         .append($("<option></option>")
-         .attr("value",key)
+         .append($('<li class="ui-widget-content" />')
          .text(value)); 
 });
+
+$('#my-select').selectable();
 
     }
   }
