2f0435b7c5a14a410d7b43fef0d745c288714817
Andrew Marder <andrew.n.marder@gmail.com>
2015-03-03 14:49:44 -0500

    Got doctests running with django-nose.

--- annotate/settings.py
+++ annotate/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
     'mirror',
     'highlighter',
 )
@@ -87,3 +88,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
--- mirror/management/commands/load.py
+++ mirror/management/commands/load.py
@@ -1,44 +1,6 @@
 from django.core.management.base import BaseCommand
-from mirror.models import File
-import pandas as pd
+from mirror.models import load
 import sys
-
-
-def index_url(folder_url):
-    '''
-    >>> index_url('http://www.sec.gov/Archives/edgar/data/769397/000076939713000018')
-    u'http://www.sec.gov/Archives/edgar/data/769397/000076939713000018/0000769397-13-000018-index.htm'
-    '''
-    df = pd.read_html(folder_url, header=0)[0]
-    df['is_index'] = df.Name.map(lambda s: hasattr(s, 'endswith') and s.endswith('index.htm'))
-    assert df.is_index.sum() == 1
-    filename = df.Name[df.is_index].values[0]
-    return '/'.join([folder_url, filename])
-
-
-def def_14a_url(folder_url):
-    '''
-    >>> def_14a_url('http://www.sec.gov/Archives/edgar/data/769397/000076939713000018')
-    u'http://www.sec.gov/Archives/edgar/data/769397/000076939713000018/proxydocument.htm'
-    '''
-    df = pd.read_html(index_url(folder_url), header=0)[0]
-    df['def_14a'] = (df.Description == 'DEF 14A') | (df.Type == 'DEF 14A')
-    assert df.def_14a.sum() == 1, df
-    filename = df.Document[df.def_14a].values[0]
-    return '/'.join([folder_url, filename])
-
-
-def load(folder):
-    folder_url = '/'.join([File.REMOTE_ROOT, folder])
-    matches = list(File.objects.filter(remote_url__startswith=folder_url))
-    count = len(matches)
-    assert count in [0, 1]
-    if count == 1:
-        return matches[0]
-    else:
-        doc_url = def_14a_url(folder_url)
-        f = File.objects.create(remote_url=doc_url)
-        return f
 
 
 class Command(BaseCommand):
@@ -51,6 +13,6 @@ class Command(BaseCommand):
             f = load(folder)
             path = f.local_path()
             counter = i + 1
-            message = '\r[%(counter)d / %(n)d - %(path)s]' % locals()
+            message = '\r[%(counter)d / %(n)d] %(path)s' % locals()
             sys.stdout.write(message)
             sys.stdout.flush()
