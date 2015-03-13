b25e840c6d9ebc1995b192c327b277be86af52a5
Andrew Marder <andrew.n.marder@gmail.com>
2015-03-13 17:02:43 -0400

    Added three new test cases.

--- random_filing.py
+++ random_filing.py
@@ -9,16 +9,22 @@ from sqlalchemy.orm import sessionmaker
 import os
 import pandas as pd
 import random
+import re
 import subprocess
 
-def random_filing(db):
-    query = 'SELECT id, type FROM filings;'
-    engine = create_engine('sqlite:///' + db)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    df = pd.read_sql(query, engine)
-    pk = int(random.choice(df.id[df.type == 'DEF 14A']))
-    return session.query(Filing).get(pk)
+class Randomizer(object):
+
+    def __init__(self, db):
+        query = 'SELECT id, type FROM filings;'
+        engine = create_engine('sqlite:///' + db)
+        Session = sessionmaker(bind=engine)
+        self.ids = pd.read_sql(query, engine)
+        self.session = Session()
+
+    def get_filing(self):
+        df = self.ids
+        pk = int(random.choice(df.id[df.type == 'DEF 14A']))
+        return self.session.query(Filing).get(pk)
 
 def clean(html):
     '''
@@ -34,9 +40,15 @@ def clean(html):
     stdoutdata, stderrdata = p.communicate(html)
     return stdoutdata
 
-db = 'db.sqlite'
-filing = random_filing(db)
-filename = re.sub(Filing.HTTP_ROOT, '', filing.url).replace('/', '-') + '.md'
-path = os.path.join('targets', filename)
-with open(path, 'w') as f:
-    f.write(clean(filing.html))
+def write(filing):
+    filename = re.sub(Filing.HTTP_ROOT, '', filing.url).replace('/', '-') + '.md'
+    path = os.path.join('targets', filename)
+    with open(path, 'w') as f:
+        f.write(clean(filing.html))
+    print path
+
+if __name__ == '__main__':
+    n = 8
+    r = Randomizer(db='db.sqlite')
+    for i in range(n):
+        write(r.get_filing())
