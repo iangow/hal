368b4dab35fc6804bcfcd5df2e9f6060ab8f716c
Andrew Marder <andrew.n.marder@gmail.com>
2015-03-13 15:49:02 -0400

    Made some progress towards creating nice target files.

--- modules.sh
+++ modules.sh
@@ -1,2 +1,5 @@
 module load Anaconda
 module load R_core
+
+module load legacy/0.0.1-fasrc01
+module load centos6/pandoc-1.12.3.3
--- random_filing.py
+++ random_filing.py
@@ -1,16 +1,37 @@
-from mirror.models import File
-from random import randint
-from django.http import HttpResponse
-from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
+'''
+Select a random DEF 14A filing from the database and print the text of
+that filing to stdout.
+'''
+import pandas as pd
+import random
+from sqlalchemy import create_engine
+from models import Filing
+from sqlalchemy.orm import sessionmaker
+import subprocess
+
+db = 'db.sqlite'
+
+def random_filing():
+    query = 'SELECT id, type FROM filings;'
+    engine = create_engine('sqlite:///' + db)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    df = pd.read_sql(query, engine)
+
+    pk = int(random.choice(df.id[df.type == 'DEF 14A']))
+    filing = session.query(Filing).get(pk)
 
 
-def random(request):
-    n = File.objects.count()
-    i = randint(0, n-1)
-    f = File.objects.all()[i]
-    path = f.path()
-    url = reverse('mirror.views.mirror', args=[path])
-    absolute_url = request.build_absolute_uri(url)
-    highlight_url = reverse('highlight') + '?url=' + absolute_url
-    return redirect(highlight_url)
+def clean(html):
+    '''
+    >>> print clean('<b>heading</b>').strip()
+    **heading**
+    '''
+    args = ['pandoc', '--from=html', '--to=markdown']
+    p = subprocess.Popen(
+        args,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    stdoutdata, stderrdata = p.communicate(html)
+    return stdoutdata
