555336bebf07dbd154f4ee21c31b100e91518027
Andrew Marder <andrew.n.marder@gmail.com>
2015-03-13 16:25:45 -0400

    Added another target.

--- random_filing.py
+++ random_filing.py
@@ -2,25 +2,23 @@
 Select a random DEF 14A filing from the database and print the text of
 that filing to stdout.
 '''
+
+from models import Filing
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
 import pandas as pd
 import random
-from sqlalchemy import create_engine
-from models import Filing
-from sqlalchemy.orm import sessionmaker
 import subprocess
 
-db = 'db.sqlite'
-
-def random_filing():
+def random_filing(db):
     query = 'SELECT id, type FROM filings;'
     engine = create_engine('sqlite:///' + db)
     Session = sessionmaker(bind=engine)
     session = Session()
     df = pd.read_sql(query, engine)
-
     pk = int(random.choice(df.id[df.type == 'DEF 14A']))
-    filing = session.query(Filing).get(pk)
-
+    return session.query(Filing).get(pk)
 
 def clean(html):
     '''
@@ -35,3 +33,10 @@ def clean(html):
     )
     stdoutdata, stderrdata = p.communicate(html)
     return stdoutdata
+
+db = 'db.sqlite'
+filing = random_filing(db)
+filename = re.sub(Filing.HTTP_ROOT, '', filing.url).replace('/', '-') + '.md'
+path = os.path.join('targets', filename)
+with open(path, 'w') as f:
+    f.write(clean(filing.html))
