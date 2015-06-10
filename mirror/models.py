280e15d05cab363974c92d2891e5e9f4b38fb695
Andrew Marder <andrew.n.marder@gmail.com>
2015-06-10 09:14:14 -0400

    Got close to registering director.director table with Django.

--- mirror/models.py
+++ mirror/models.py
@@ -4,6 +4,37 @@ import os
 from sec_utils import Filing
 
 
+class Directors(models.Model):
+    '''
+    This model was created by inspected the director.director table on
+    Ian's server. Let's use the admin to make sure that django can
+    read data from the server.
+    '''
+    
+    director_id = models.TextField(blank=True)
+    company = models.TextField(blank=True)
+    director = models.TextField(blank=True)
+    ticker = models.TextField(blank=True)
+    fy_end = models.DateField(blank=True, null=True)
+    gender = models.TextField(blank=True)
+    age = models.IntegerField(blank=True, null=True)
+    chairman = models.NullBooleanField()
+    vice_chairman = models.NullBooleanField()
+    lead_independent_director = models.NullBooleanField()
+    audit_committee_financial_expert = models.NullBooleanField()
+    start_date = models.DateField(blank=True, null=True)
+    term_end_date = models.DateField(blank=True, null=True)
+    tenure_yrs = models.FloatField(blank=True, null=True)
+    num_committees = models.IntegerField(blank=True, null=True)
+    committees = models.TextField(blank=True)
+    fileyear = models.IntegerField(blank=True, null=True)
+    insider_outsider_related = models.TextField(blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'director\".\"director'
+
+
 class File(models.Model):
 
     REMOTE_ROOT = 'http://www.sec.gov/Archives/edgar/data'
