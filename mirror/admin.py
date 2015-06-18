bbdc7e90c93facffd1bac92c19d39ca9ac1dcd15
Andrew Marder <andrew.n.marder@gmail.com>
2015-06-18 08:51:53 -0400

    Wrote inefficient code to load biographies into the database.

--- mirror/admin.py
+++ mirror/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Directors, Filing
-import json
+from .models import Directors, Filing, Biography
+from .es import bios, clean
 
 
 class FilingAdmin(admin.ModelAdmin):
@@ -12,5 +12,30 @@ class FilingAdmin(admin.ModelAdmin):
         self.message_user(request, 'Added %d filings.' % count)
 
 
+def load_biographies():
+    count = 0
+    for d in bios():
+        d['filing'] = Filing.objects.get(folder=d['filing'])
+        try:
+            b = Biography.objects.get(
+                filing=d['filing'], director_name=d['director_name']
+            )
+        except Biography.DoesNotExist:
+            d['text'] = clean(d['text'])
+            b = Biography.objects.create(**d)
+            count += 1
+    return count
+
+
+class BiographyAdmin(admin.ModelAdmin):
+
+    actions = ['load']
+
+    def load(self, request, queryset):
+        count = load_biographies()
+        self.message_user(request, 'Added %d biographies.' % count)
+
+
 admin.site.register(Directors)
 admin.site.register(Filing, FilingAdmin)
+admin.site.register(Biography, BiographyAdmin)
