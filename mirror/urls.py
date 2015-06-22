c07401144543b15a6506a6c13b1f12f7c4793b89
Andrew Marder <andrew.n.marder@gmail.com>
2015-06-22 08:47:33 -0400

    Fixed url pattern.

--- mirror/urls.py
+++ mirror/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'/?$', views.home),
+    url(r'^/?$', views.home),
     url(r'random$', views.random_filing),
     url(r'highlight/(?P<folder>.*)$', 'mirror.views.highlight', name='highlight'),
     url(r'filing/(?P<folder>.*)$', views.mirror, name='filing'),
