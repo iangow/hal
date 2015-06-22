eff83cce506c37c2834ef848756b7c6def413872
Andrew Marder <andrew.n.marder@gmail.com>
2015-06-22 09:07:45 -0400

    Added email settings.

--- annotate/settings.py
+++ annotate/settings.py
@@ -83,3 +83,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Email settings
+EMAIL_HOST = 'smtp.postmarkapp.com'
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '25'))
+POSTMARK_API_KEY = os.environ.get('POSTMARK_API_KEY')
+EMAIL_HOST_USER = POSTMARK_API_KEY
+EMAIL_HOST_PASSWORD = POSTMARK_API_KEY
+SERVER_EMAIL = 'amarder@hbs.edu'
+
+ADMINS = (
+    ('Andrew Marder', 'amarder@hbs.edu'),
+)
+
+MANAGERS = ADMINS
