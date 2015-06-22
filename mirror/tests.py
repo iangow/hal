772a7f83eb76cc149e5954c2c6cb19e4b6d47551
Andrew Marder <andrew.n.marder@gmail.com>
2015-06-22 11:30:39 -0400

    Got tests passing.

--- mirror/tests.py
+++ mirror/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from models import File
+from models import Filing
 import os
 from django.test import Client
 from django.core.urlresolvers import reverse
@@ -9,17 +9,15 @@ import views
 class MyTestCase(TestCase):
 
     def setUp(self):
-        self.url = 'http://www.sec.gov/Archives/edgar/data/769397/000076939713000018/0000769397-13-000018.hdr.sgml'
-        self.f = File.objects.create(remote_url=self.url)
+        self.folder = '769397/000076939713000018'
+        self.f = Filing.objects.create(folder=self.folder)
 
     def test_download(self):
         self.f.download()
-        os.remove(self.f.local_path())
 
     def test_mirror(self):
         c = Client()
-        path = self.f.remote_url.replace(File.REMOTE_ROOT, '')
+        path = self.folder
         url = reverse(views.mirror, args=[path])
         response = c.get(url)
         self.assertEquals(response.status_code, 200)
-        os.remove(self.f.local_path())
