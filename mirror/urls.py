a6ff5595e56999e499390d784d87da883fa2b577
Andrew Marder <andrew.n.marder@gmail.com>
2015-06-22 09:46:18 -0400

    Being more specific about url patterns.

--- mirror/urls.py
+++ mirror/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^/?$', views.home),
-    url(r'random$', views.random_filing),
-    url(r'highlight/(?P<folder>.*)$', 'mirror.views.highlight', name='highlight'),
-    url(r'filing/(?P<folder>.*)$', views.mirror, name='filing'),
+    url(r'^random$', views.random_filing),
+    url(r'^highlight/(?P<folder>\d+/\d+)$', 'mirror.views.highlight', name='highlight'),
+    url(r'^filing/(?P<folder>\d+/\d+)$', views.mirror, name='filing'),
 )
