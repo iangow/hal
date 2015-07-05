fdacac1c6829ae7feff3a02e080fcb17c4508c3b
Andrew Marder <andrew.n.marder@gmail.com>
2015-07-05 10:39:48 -0400

    Made table for joining highlights with other directorships.

---
+++ mirror/create_crosswalk.sql
@@ -0,0 +1,15 @@
+WITH x AS
+( 
+       SELECT regexp_replace(file_name, e'^edgar/data/(\\d+)/(\\d{10})-(\\d{2})-(\\d{6})\\.txt$', e'\\1/\\2\\3\\4') AS folder, 
+              equilar_id, 
+              fy_end 
+       FROM   director.equilar_proxies ) 
+SELECT folder, 
+       director_id, 
+       director 
+INTO   crosswalk 
+FROM   director.director 
+JOIN   x 
+ON     director.equilar_id(director_id)=equilar_id 
+AND    director.director.fy_end=x.fy_end 
+AND    folder <> '';
