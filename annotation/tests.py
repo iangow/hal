0f5988821c487524a83faabf5a9d388e4ce5af1f
Andrew Marder <andrew.n.marder@gmail.com>
2015-07-30 10:01:16 -0400

    Got CRUD tests working.

--- annotation/tests.py
+++ annotation/tests.py
@@ -1,38 +1,50 @@
 from django.test import TestCase, Client
 from models import Annotation
+import json
 
 
 class AnnotationTests(TestCase):
 
-    def _check_count(self, n):
-        self.assertEquals(Annotation.objects.count(), n)
-
-    def test_search(self):
-        pass
-
-    def test_create(self):
-        data = {
+    def setUp(self):
+        self.data = {
             "id": 1,
             "quote": "Test quote.",
             "ranges": "[{u'start': u'/p', u'end': u'/p', u'startOffset': 1, u'endOffset': 10}]",
             "text": "",
             "uri": "/highlight/101271/000110465907029331"
         }
-        path = '/annotations'
-        response = Client().post(path, data)
+        self._check_count(0)
+        self._create_annotation()
         self._check_count(1)
+
+    def _check_count(self, n):
+        self.assertEquals(Annotation.objects.count(), n)
+
+    def _create_annotation(self):
+        path = '/annotations'
+        Client().post(path, self.data)
+
+    def test_search(self):
+        pass
+
+    def test_create(self):
         a = Annotation.objects.all()[0]
-        self.assertEquals(a.quote, data['quote'])
+        self.assertEquals(a.quote, self.data['quote'])
 
     def test_read(self):
-        pass
+        response = Client().get('/annotations/1')
+        response_data = json.loads(response.content)
+        self.assertEquals(response_data['quote'], self.data['quote'])
 
     def test_update(self):
-        pass
+        new_quote = 'Hello World!'
+        data = self.data.copy()
+        data['quote'] = new_quote
+        response = Client().put('/annotations/1', json.dumps(data), content_type='application/json')
+        self._check_count(1)
+        a = Annotation.objects.all()[0]
+        self.assertEquals(a.quote, new_quote)
 
     def test_delete(self):
-        self._check_count(0)
-        self.test_create()
-        self._check_count(1)
         Client().delete('/annotations/1')
         self._check_count(0)
